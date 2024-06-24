85b10686da868ab8427aec30d4cf3ed4bb802f10
hauntednuke <briangonzalez.9406@gmail.com>
2024-06-24 16:46:19 -0400

    librerias

--- comunes/head.php
+++ comunes/head.php
@@ -1,1 +1,5 @@
+<link rel="stylesheet" href="comunes/css/normalize.css">
+<link rel="stylesheet" href="comunes/css/bootstrap.min.css">
 <link rel="stylesheet" href="comunes/css/estilos.css">
+<script src="comunes/js/bootstrap.bundle.min.js"></script>
+<script src="comunes/js/datatables.min.js"></script>
